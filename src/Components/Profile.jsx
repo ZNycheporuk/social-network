4c4c8d10edbc34910d433a4402d2d8bdc6754f76
ZNycheporuk <nycheporuk.zakhar@gmail.com>
2020-07-05 12:10:25 +0300

    init

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img alt='Panorama' src='https://upload.wikimedia.org/wikipedia/commons/1/11/B%C3%B6dele_Bregenzerwald_Panorama.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile;
